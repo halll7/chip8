8ae8d70011e215c2e37f87901f92957a8b11f4b6
Leigh Hall <leighbh@gmail.com>
2018-07-17 21:37:50 +0100

    Initial Commit

---
+++ chip8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  chip8
+//
+//  Created by Leigh Hall on 17/07/2018.
+//  Copyright © 2018 Leigh Hall. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
